411cab83ccb827e95050425afbcdac706cdb96ca
FURGOL Giuliano (E0041767) <g.furgol@gmail.com>
2018-12-14 13:02:04 +0100

    Test Heroku Deploy

---
+++ build/precache-manifest.b92c6ce7872a827bc8b29ef80cd09eca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e834b3edf6be80489b1",
+    "url": "/static/js/main.0e834b3e.chunk.js"
+  },
+  {
+    "revision": "4884e20140e1b718aace",
+    "url": "/static/js/1.4884e201.chunk.js"
+  },
+  {
+    "revision": "0e834b3edf6be80489b1",
+    "url": "/static/css/main.9f0e7846.chunk.css"
+  },
+  {
+    "revision": "331a4be4941c0d1006d2958618b83636",
+    "url": "/index.html"
+  }
+];
